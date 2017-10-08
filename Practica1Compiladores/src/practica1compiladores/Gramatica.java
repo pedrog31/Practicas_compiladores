e678136c21c496c9bbeedff97b92c571ce15e462
Pedro Gallego <pedro.gallego@udea.edu.co>
2017-10-08 14:44:11 -0500

    2.	Identificar si es posible simplificar la gramática ingresada: a.	En caso Afirmativo: i.	Mostrar en pantalla los conjuntos de No Terminales vivos, muertos e inalcanzables. ii.	Mostrar la gramática luego del proceso de simplificación. ok

--- Practica1Compiladores/src/practica1compiladores/Gramatica.java
+++ Practica1Compiladores/src/practica1compiladores/Gramatica.java
@@ -18,6 +18,10 @@ public class Gramatica {
     private int nNoTerminales;
     private int nTerminales;
     private Boolean isRegular;
+    
+    public Gramatica() {
+        producciones = new ArrayList();
+    }
 
     public int getnNoTerminales() {
         return nNoTerminales;
@@ -36,17 +40,13 @@ public class Gramatica {
     }
 
     public Boolean getIsRegular() {
-        return producciones.stream().noneMatch((produccion) -> (!produccion.isSpecial()));
+       return producciones.stream().noneMatch((produccion) -> (!produccion.isSpecial()));
     }
 
     public void setIsRegular(Boolean isRegular) {
         this.isRegular = isRegular;
     }
-
-    public Gramatica() {
-        producciones = new ArrayList();
-    }
-
+    
     public List<Produccion> getProducciones() {
         return producciones;
     }
@@ -71,5 +71,114 @@ public class Gramatica {
             }
         } else producciones = null;
     }
+    
+    public Set<String> getNoTerminalesAlcanzables () {
+        Set<String> alcanzables = new HashSet();
+        alcanzables.add(producciones.get(0).getPartes(0));
+        alcanzables = addAlcanzables(alcanzables);
+        return alcanzables;
+    }
+    
+    public Set<String> getNoTerminalesMuertos (Set<String> vivos){
+        Set<String> muertos;
+        muertos = getAllNoTerminales();
+        muertos.removeAll(vivos);
+        return muertos;
+    }
+    
+    public Set<String> getNoTerminalesVivos (){
+        Set<String> vivos = new HashSet();
+        vivos = addVivos(vivos);
+        return vivos;
+    }
+    
+    public Set<String> getAllNoTerminales () {
+        Set<String> noTerminales = new HashSet();
+        producciones.forEach((produccion) -> {
+            noTerminales.add(produccion.getPartes(0));
+        });
+        return noTerminales;
+    }
 
+    private Set<String> addVivos(Set<String> vivos) {
+        Boolean added;
+        for (Produccion produccion: producciones) {
+            added = true;
+            for (String part: produccion.getRightParts()) {
+                if (!(vivos.contains(part) || produccion.isTerminal(part))){
+                    added = false;
+                    break;
+                }
+            }
+            if (added && vivos.add(produccion.getPartes(0))){
+             vivos = addVivos(vivos);
+             break;
+            }
+        }
+        return vivos;
+    }
+
+    private Set<String> addAlcanzables(Set<String> alcanzables) {
+        int size1 = alcanzables.size();
+        for (Produccion produccion: producciones) {
+            if (alcanzables.contains(produccion.getPartes(0))) {
+                for (String part: produccion.getRightParts()){
+                    if(produccion.isNonTerminal(part)){
+                        alcanzables.add(part);
+                    }
+                }
+            }else{
+                if (alcanzables.size() > size1)
+                    alcanzables = addAlcanzables(alcanzables);
+                break;
+            }
+        }
+        return alcanzables;
+    }
+    
+    public Set<String> getNoTerminalesInalcanzables (Set<String> alcanzables){
+        Set<String> inalcanzables;
+        inalcanzables = getAllNoTerminales();
+        inalcanzables.removeAll(alcanzables);
+        return inalcanzables;
+    }
+    
+    public Gramatica simplificarGramatica (Set<String> muertos, Set<String> inalcanzables) {
+        Gramatica gramatica = new Gramatica();
+        Boolean eliminable;
+        for (Produccion produccion: producciones) {
+            eliminable = false;
+            List<String> partes = produccion.getPartes();
+            for (String muerto: muertos) {
+                if (partes.contains(muerto)){
+                    eliminable = true;
+                    break;
+                }
+            }
+            for (String inalcanzable: inalcanzables) {
+                if (partes.contains(inalcanzable)){
+                    eliminable = true;
+                    break;
+                }
+            }
+            if (!eliminable) {
+                Produccion produccionNueva = new Produccion(gramatica.getProducciones().size()+1);
+                produccionNueva.setPartes(produccion.getPartes());
+                gramatica.getProducciones().add(produccionNueva);
+            }
+                
+        }
+        return gramatica;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder gra = new StringBuilder();
+        producciones.forEach((produccion) -> {
+            gra.append(produccion.toString()).append("\n");
+        });
+        return gra.toString();
+    }
+
+    
 }
